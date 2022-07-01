2b8387d0dbff2947f4d9dd12d5b837a85bbb5526
Denny Rizky Ramdani <denny@DESKTOP-LMN9F9I.localdomain>
2022-07-01 21:22:57 +0700

    add delete function

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -2,31 +2,53 @@ import { useEffect, useState } from 'react'
 import './Todo.css'
 
 function TodoItems(props) {
-    let checkedtoggle = (props.status) ? "Checked" : ""
+    let checkedtoggle = (props.status) ? ["Checked","Selected"] : Array(2).fill("")
     return(
-        <div className="TodoContainer" onClick={props.onClick}>
-            <input type="checkbox"></input>
-            <p className={`ItemDesc ${checkedtoggle}`}>{props.text}</p>
+        <div className={`TodoContainer ${checkedtoggle[1]}`} onClick={props.onClick}>
+            {/* <input type="checkbox"></input> */}
+                <p className={`ItemDesc ${checkedtoggle[0]}`}>{props.text}</p>
         </div>
     )
 }
 
 function Todo() {
+    // type DataFormat = {id:number,description:string,checked:boolean}
     const originData = [
         {id:1,description:"item1",checked:false},
         {id:2,description:"item2",checked:false},
         {id:3,description:"item3",checked:false},
     ]
 
-    // let data = [...originData]
-
     const [data,setData] = useState(originData)
 
     
     function FlipCheck(index,checkstatus){
+        console.log(index)
         let temporaryFlipData=data
         temporaryFlipData[(index-1)].checked=checkstatus
         console.log(temporaryFlipData[(index-1)])
+        setData([...temporaryFlipData])
+    }
+
+    function FlipCheckResetAll(){
+        let temporaryFlipData=data
+        for (let i of temporaryFlipData) {
+            i.checked=false
+        }
+        setData([...temporaryFlipData])
+    }
+
+    function DeleteData(){
+        let temporaryFlipData=[]
+        let count=1
+        for (let i of data) {
+            if (i.checked === false) {
+                i.id = count
+                temporaryFlipData.push(i)
+                count++
+            }
+        }
+        console.log(temporaryFlipData)
         setData([...temporaryFlipData])
     }
 
@@ -38,7 +60,10 @@ function Todo() {
     return(
         <div className='Todo'>
             {dataTodoComponent}
-            <div className='ResetButton'><button>Reset</button></div>
+            <div className='ButtonGroup'>
+                <button className="Deleter" onClick={()=>DeleteData()}>Delete</button>
+                <button className="Reseter" onClick={()=>FlipCheckResetAll()}>Reset</button>
+            </div>
         </div>
     )
 
