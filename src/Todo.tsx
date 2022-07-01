8dff117430c394776b4465ffdc68378723e39bd6
Denny Rizky Ramdani <denny@DESKTOP-LMN9F9I.localdomain>
2022-07-01 17:57:46 +0700

    Working checklist & state

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,27 +1,48 @@
+import { useEffect, useState } from 'react'
 import './Todo.css'
 
 function TodoItems(props) {
+    let checkedtoggle = (props.status) ? "Checked" : ""
     return(
-        <div className="TodoContainer">
+        <div className="TodoContainer" onClick={props.onClick}>
             <input type="checkbox"></input>
-            <p className='ItemDesc'>{props.text}</p>
+            <p className={`ItemDesc ${checkedtoggle}`}>{props.text}</p>
         </div>
     )
 }
 
 function Todo() {
-    const data = ["item1","item2","bitsch","NIG"]
-    let dataTodo = []
-        for (let i of data) {
-            dataTodo.push(<TodoItems text={i} />)
-            console.log(i)
-        }
+    const originData = [
+        {id:1,description:"item1",checked:false},
+        {id:2,description:"item2",checked:false},
+        {id:3,description:"item3",checked:false},
+    ]
+
+    // let data = [...originData]
+
+    const [data,setData] = useState(originData)
+
+    
+    function FlipCheck(index,checkstatus){
+        let temporaryFlipData=data
+        temporaryFlipData[(index-1)].checked=checkstatus
+        console.log(temporaryFlipData[(index-1)])
+        setData([...temporaryFlipData])
+    }
+
+    let dataTodoComponent = []
+    for (let i of data) {
+        dataTodoComponent.push(<TodoItems text={i.description} status={i.checked} onClick={()=>FlipCheck(i.id,!i.checked)}/>)
+    }
+
     return(
         <div className='Todo'>
-            {dataTodo}
+            {dataTodoComponent}
             <div className='ResetButton'><button>Reset</button></div>
         </div>
     )
+
+    
 }
 
 
