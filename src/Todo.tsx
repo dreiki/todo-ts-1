2c91a01f4138c8cb9049c014dc89e8a034d71a0f
Denny Rizky Ramdani <denny@DESKTOP-LMN9F9I.localdomain>
2022-07-01 22:50:40 +0700

    add input function

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -11,6 +11,16 @@ function TodoItems(props) {
     )
 }
 
+function TodoItemsInput(props) {
+    return(
+        <div className={`TodoContainer`}>
+            <input className="InvisibleInput" name="Input" value={props.value} onInput={props.onChange}></input>
+            <button className="Normal" onClick={props.onClick}> Add</button>
+            {/* <p className={`ItemDesc ${checkedtoggle[0]}`}>{props.text}</p> */}
+        </div>
+    )
+}
+
 function Todo() {
     // type DataFormat = {id:number,description:string,checked:boolean}
     const originData = [
@@ -20,6 +30,7 @@ function Todo() {
     ]
 
     const [data,setData] = useState(originData)
+    const [input,setInput] = useState("")
 
     
     function FlipCheck(index,checkstatus){
@@ -52,6 +63,15 @@ function Todo() {
         setData([...temporaryFlipData])
     }
 
+    function AddNewData(newdata){
+        if (newdata != "") {
+            let temporaryData=data
+            temporaryData.push({id:temporaryData.length+1,description:newdata,checked:false})
+            console.log(temporaryData)
+            setData([...temporaryData])
+        }
+    }
+
     let dataTodoComponent = []
     for (let i of data) {
         dataTodoComponent.push(<TodoItems text={i.description} status={i.checked} onClick={()=>FlipCheck(i.id,!i.checked)}/>)
@@ -60,9 +80,10 @@ function Todo() {
     return(
         <div className='Todo'>
             {dataTodoComponent}
+            <TodoItemsInput onClick={()=>AddNewData(input)} onChange={(e)=>setInput(e.target.value)}/>
             <div className='ButtonGroup'>
                 <button className="Deleter" onClick={()=>DeleteData()}>Delete</button>
-                <button className="Reseter" onClick={()=>FlipCheckResetAll()}>Reset</button>
+                <button className="Normal" onClick={()=>FlipCheckResetAll()}>Reset</button>
             </div>
         </div>
     )
